86ff890d04a10ef66a2504626160ca32b16d29a7
Valentin Guaquiere <valentin.guaquiere@orange.fr>
2026-07-15 16:23:30 +0200

    débu de la section hero

--- index.php
+++ index.php
@@ -2,15 +2,17 @@
 $pageTitle = "Accueil";
 require_once 'composants/header.php';
 ?>
-<section class="section-hero row">
-    <div class="content-hero col-6 ">
-        <h1 class="text-center">Devenez bénévole dans une association avec Benenova</h1>
+<section class="section-hero row g-0">
+    <div class="content-hero col-lg-6 d-flex flex-column justify-content-center">
+        <h1>Devenez bénévole dans une association avec Benenova</h1>
         <p>Rejoignez Benenova pour participer à des missions de bénévolat concrètes, accessibles à tous, près de chez
-            vous et adaptées à votre rythme.En quelques clics, découvrez des actions utiles, engagez-vous ponctuellement
+            vous et adaptées à votre rythme. En quelques clics, découvrez des actions utiles, engagez-vous ponctuellement
             ou régulièrement, et contribuez à des projets associatifs qui ont un impact réel sur votre territoire.</p>
     </div>
-    <div class="col-6">
-        <img src="assets/equipe-benenova.png" alt="Hero Image" class="img-fluid">
+    <div class="col-lg-6 p-0">
+        <div class="hero-image">
+            <img src="assets/equipe-benenova.png" alt="Hero Image">
+        </div>
     </div>
 </section>
 
